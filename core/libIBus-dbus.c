1b20a766bad8d6b7e79a81d109a1a0dd360cce1a
Arun Madhavan <arun_madhavan@comcast.com>
2025-08-07 14:26:16 -0400

    RDKEMW-6588: fix the compilation error
    Reason for Change: fix the below compilation error
    | ../../git/core/libIBus-dbus.c:1191:25: error: 'void*' is not a pointer-to-object type
    | 1191 | ctx->handler(eventData->owner, eventData->id, (void *)&eventData->data, eventData->len);

--- core/libIBus-dbus.c
+++ core/libIBus-dbus.c
@@ -1188,10 +1188,9 @@ static void _EventHandler_FuncWrapper (void *ctx, void *arg)
 		{
                     //log("Event Handler [%s]for Event [%d] will be  invoked\r\n", eventData->owner, eventData->id);
                     if (cctx->handler != NULL)
-                        ctx->handler(eventData->owner, eventData->id, (void *)&eventData->data, eventData->len);
+                        ((IARM_Bus_EventContext_t *)ctx)->handler(eventData->owner, eventData->id, (void *)&eventData->data, eventData->len);
                 }
             }
-	/*coverity[dead_error_line]*/
         } while ((event_list = g_list_next(event_list)) != NULL);
     }
     IBUS_Unlock(lock);
