a1dff658155e8ca3cb07638a6f029c0952d89611
Arun Madhavan <arun_madhavan@comcast.com>
2025-08-07 13:57:37 -0400

    RDKEMW-6588: fix coverity error CID UNUSED_VALUE
    Reason for Change: fix the below mentioned coverity error
    Assigning value "NULL" to "cctx" here, but that stored value is overwritten before it can be used.

--- core/libIBus-dbus.c
+++ core/libIBus-dbus.c
@@ -1191,10 +1191,6 @@ static void _EventHandler_FuncWrapper (void *ctx, void *arg)
                         ctx->handler(eventData->owner, eventData->id, (void *)&eventData->data, eventData->len);
                 }
             }
-            else
-            {
-                cctx = NULL;
-            }
         } while ((event_list = g_list_next(event_list)) != NULL);
     }
     IBUS_Unlock(lock);
