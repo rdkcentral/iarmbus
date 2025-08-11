6b6f920098ebf16c5ecd7a35a3873a1d866e8cc8
Arun Madhavan <arun_madhavan@comcast.com>
2025-08-11 19:38:40 -0400

    RDKEMW-6588: Return INVALID_PARAM when Name is NULL

--- core/libIBusDaemon-dbus.c
+++ core/libIBusDaemon-dbus.c
@@ -150,7 +150,10 @@ static IARM_Result_t _RegisterMember(void *arg)
     }
 
     //log("Entering [%s] - [%s]\r\n", __FUNCTION__, member->selfName);
-
+    if (member->selfName == NULL || strlen(member->selfName) == 0) {
+	    free(member);
+	    return IARM_RESULT_INVALID_PARAM;
+    }
     if ((strlen(member->selfName)) && _IsRegistered(member->selfName)) {
         log("Found and Unregistering old instance of client - [%s]\r\n", member->selfName);
         _UnRegisterMember (member);
